7815322d985030a3668813d156a601ed95f69af6
OWO Devs <developers.owo@gmail.com>
2025-07-04 14:19:27 +0200

    chore: add more events using sounds

--- ModClass.cs
+++ ModClass.cs
@@ -1,4 +1,6 @@
-﻿using Modding;
+﻿using GlobalEnums;
+using Modding;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UObject = UnityEngine.Object;
@@ -11,6 +13,10 @@ namespace OWO_HollowKnight
         public override string GetVersion() => "v0.0.1";
         public OWOSkin owoSkin;
         internal static OWO_HollowKnight Instance;
+
+        //--
+        string playerMove = "";
+        //--
         
         public bool IsSet()
         {
@@ -25,16 +31,64 @@ namespace OWO_HollowKnight
             owoSkin = new OWOSkin();
             
             Log("Initialized");
-            
-            //ModHooks.HeroUpdateHook += OnHeroUpdate; 
+
+            //Patch methods            
+            On.HeroAudioController.PlaySound += OnHeroSounds;
+            On.HeroController.DoDoubleJump += OnDoubleJump;
+            ModHooks.AttackHook += OnAttack;            
         }
 
-        private void OnHeroUpdate()
+        private void PreFeel(string sensationName)
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            { 
-                LogWarn("PRESSING SPACE");
+            Log("Feel:" + sensationName);
+            owoSkin.Feel(sensationName);
+        }
+
+        #region Hero
+        private void OnAttack(AttackDirection direction)
+        {
+            Log($"Attack {direction.ToString()}");
+        }
+
+        private void OnDoubleJump(On.HeroController.orig_DoDoubleJump orig, HeroController self)
+        {
+            orig(self);
+            PreFeel("Double Jump");
+        }
+
+        private void OnHeroSounds(On.HeroAudioController.orig_PlaySound orig, HeroAudioController self, HeroSounds soundEffect)
+        {
+            orig(self, soundEffect);
+
+            switch (soundEffect)
+            {
+                case HeroSounds.JUMP:
+                    PreFeel("Jump");
+                    break;
+                case HeroSounds.DASH:
+                    PreFeel("Dash");
+                    break;
+                case HeroSounds.SOFT_LANDING:
+                    PreFeel("Soft Landing");
+                    break;
+                case HeroSounds.HARD_LANDING:
+                    PreFeel("Hard Landing");
+                    break;
+                case HeroSounds.FALLING:
+                    PreFeel("Falling");
+                    break;
+                case HeroSounds.WALLJUMP:
+                    PreFeel("Wall Jump");
+                    break;
+                case HeroSounds.WALLSLIDE:
+                    PreFeel("Wall Slide");
+                    break;
+                case HeroSounds.TAKE_HIT:
+                    PreFeel("Take Hit");
+                    break;
             }
         }
+
+        #endregion
     }
 }
